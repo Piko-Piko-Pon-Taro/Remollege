a6078285951d8ffd23f2c527a789136595cb0f90
渡辺豪志 <tuyosi.628@gmail.com>
2020-08-14 12:32:41 +0900

    linterテスト

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -66,5 +66,10 @@
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-export default {}
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
 </script>
