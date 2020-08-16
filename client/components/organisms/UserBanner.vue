adf6226a73f36cc39d7213d1e4f8eb848564f143
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-08-16 13:00:16 +0900

    [update]video area
    ビデオの下に名前を表示するためのコンポーネント追加

---
+++ client/components/organisms/UserBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card :color="$const.MAIN_COLOR" dark class="px-5 my-2" height="50px">
+    <v-row>
+      <UserIcon :src="img" class="mx-1 my-1" />
+      <v-card-title class="py-0">{{ name }}</v-card-title>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  components: {
+    UserIcon: () => import('@/components/atoms/UserIcon')
+  },
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    img: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
