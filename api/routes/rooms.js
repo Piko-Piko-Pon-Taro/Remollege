c48d49df7fb3792b782e9664919d65a3a9c699b3
渡辺豪志 <tuyosi.628@gmail.com>
2020-08-21 23:40:00 +0900

    roomsRoute変更

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -3,27 +3,54 @@ var router = express.Router();
 const models = require(global.models);
 const Room = models.Room;
 
-/* GetRoomTables */
-router.get("/:id/tables/", async (req, res) => {
+/* GetOneRoom */
+router.get("/:id", async (req, res) => {
   try {
-    const { Tables: tables } = await Room.scope("withTables").findByPk(
-      req.params.id
-    );
-    res.json({ tables });
+    let room = await Room.scope('withUsers').findByPk(req.params.id);
+    tables = room.Tables.map(t => {
+      return {
+        id: t.id,
+        roomId: t.roomId,
+        createdAt: t.createdAt,
+        users: t.TableUsers.map(tu => {
+          return tu.User
+        })
+      };
+    })
+    res.json({ room: {
+      id: room.id,
+      buildingId: room.buildingId,
+      name: room.name,
+      createdAt: room.createdAt,
+      updatedAt: room.updatedAt,
+      tables
+    } });
   } catch (e) {
     res.json(e.message);
   }
 });
 
-/* GetRoomActiveUsers */
-router.get("/:id/users/", async (req, res) => {
-  try {
-    let users = await Room.scope("withUsers").findByPk(req.params.id);
-    users = users.Tables.map((t) => t.TableUser.User);
-    res.json({ users });
-  } catch (e) {
-    res.json(e.message);
-  }
-});
+// /* GetRoomTables */
+// router.get("/:id/tables/", async (req, res) => {
+//   try {
+//     const { Tables: tables } = await Room.scope("withTables").findByPk(
+//       req.params.id
+//     );
+//     res.json({ tables });
+//   } catch (e) {
+//     res.json(e);
+//   }
+// });
+
+// /* GetRoomActiveUsers */
+// router.get("/:id/users/", async (req, res) => {
+//   try {
+//     let users = await Room.scope("withUsers").findByPk(req.params.id);
+//     users = users.Tables.map((t) => t.TableUser.User);
+//     res.json({ users });
+//   } catch (e) {
+//     res.json(e);
+//   }
+// });
 
 module.exports = router;
