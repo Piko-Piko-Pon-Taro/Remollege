2036edcf11f5442374f1423d854a2b1e4bc9be17
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-09-04 21:28:09 +0900

    [update]storybook
    introductionとexampleを削除

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,5 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
