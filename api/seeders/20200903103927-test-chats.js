e36d9ce31d19b41a9720c804824dae34bd2f5241
Yuki Nishii <dubhemerakphecdamegrez@gmail.com>
2020-09-05 21:47:44 +0900

    set id & delete init data

--- api/seeders/20200903103927-test-chats.js
+++ api/seeders/20200903103927-test-chats.js
@@ -3,11 +3,11 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Chats', [ 
-      { tableId:1, userId:1, content:'ぴこぴこポンはおいしいね！', createdAt:'2020-08-14 00:00:00', updatedAt:'2020-08-14 00:00:00'},
-      { tableId:1, userId:4, content:'ヤサイニンニクアブラ', createdAt:'2020-08-14 13:57:57', updatedAt:'2020-08-14 13:57:57'},
-      { tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 13:58:51', updatedAt:'2020-08-14 13:58:51'},
-      { tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 15:21:11', updatedAt:'2020-08-14 15:21:11'},
-      { tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 17:10:08', updatedAt:'2020-08-14 17:10:08'}
+      { id:1, tableId:1, userId:1, content:'ぴこぴこポンはおいしいね！', createdAt:'2020-08-14 00:00:00', updatedAt:'2020-08-14 00:00:00'},
+      { id:2, tableId:1, userId:4, content:'ヤサイニンニクアブラ', createdAt:'2020-08-14 13:57:57', updatedAt:'2020-08-14 13:57:57'},
+      { id:3, tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 13:58:51', updatedAt:'2020-08-14 13:58:51'},
+      { id:4, tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 15:21:11', updatedAt:'2020-08-14 15:21:11'},
+      { id:5, tableId:1, userId:1, content:'ヤサイマシマシニンニクマシマシアブラマシマシ', createdAt:'2020-08-14 17:10:08', updatedAt:'2020-08-14 17:10:08'}
     ], {});
   },
 
--- api/seeders/20200903103932-test-tables.js
+++ api/seeders/20200903103932-test-tables.js
@@ -3,15 +3,15 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Tables', [ 
-      { roomId:1, createdAt:'2020-08-14 00:00:00', updatedAt:'2020-08-14 00:00:00'},
-      { roomId:1, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:1, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
-      { roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'}
+      { id:1, roomId:1, createdAt:'2020-08-14 00:00:00', updatedAt:'2020-08-14 00:00:00'},
+      { id:2, roomId:1, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:3, roomId:1, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:4, roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:5, roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:6, roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:7, roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:8, roomId:3, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'},
+      { id:9, roomId:2, createdAt:'2020-08-15 00:00:00', updatedAt:'2020-08-15 00:00:00'}
     ], {});
   },
 
