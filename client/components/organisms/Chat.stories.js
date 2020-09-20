3600d38c82f1c6d7b52bf9d45a9e7ce2f4d6503b
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-09-20 11:40:56 +0900

    [add]Chat
    Chat用コンポーネントの追加

---
+++ client/components/organisms/Chat.stories.js
@@ -0,0 +1,43 @@
+import { base, filename } from 'paths.macro'
+import Chat from './Chat.vue'
+
+export default {
+  title: `${base.replace('/components/', '')}${filename.replace(
+    '.stories',
+    ''
+  )}`,
+  component: Chat,
+  argTypes: {
+    onClick: { action: 'clicked' }
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Chat },
+  template: '<Chat @click="onClick" v-bind="$props"/>'
+})
+
+const messages = [
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+  {id:1, name: "ピコピコポン太郎", text: "こんにちは", img: "https://storage.googleapis.com/remollege-storage/static/waseda.jpg"},
+]
+
+export const Default = Template.bind({})
+Default.args = {
+  drawer: true,
+  messages: messages
+}
---
+++ client/components/organisms/Chat.vue
@@ -0,0 +1,93 @@
+<template>
+<v-navigation-drawer
+      :value="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      dark
+      right
+      width="600"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            CHAT
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card :color="$const.BASE_COLOR2" tile>
+          <v-timeline light dense>
+              <v-virtual-scroll
+      :items="messages"
+      :item-height="100"
+      height="800"
+    >
+    <template v-slot="{ item }">
+    <v-timeline-item
+      large
+      fill-dot
+    >
+      <template v-slot:icon>
+          <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        
+        <v-avatar v-bind="attrs"
+          v-on="on">
+          <img :src="item.img">
+        </v-avatar>
+      </template>
+      <span>{{item.name}}</span>
+    </v-tooltip>
+      </template>
+      <v-card class="elevation-2 mr-5">
+        <v-card-text>{{item.text}}</v-card-text>
+      </v-card>
+    </v-timeline-item>
+    </template>
+    </v-virtual-scroll>
+  </v-timeline>
+<!-- <v-timeline dense>
+      <v-timeline-item
+        v-for="n in 2"
+        :key="n"
+      >
+      <template v-slot:icon>
+        <v-avatar>
+          <img src="https://storage.googleapis.com/remollege-storage/static/waseda.jpg">
+        </v-avatar>
+      </template>
+      <template v-slot:opposite>
+        <span>ピコピコポン太郎</span>
+      </template>
+        <span slot="opposite">Tus eu perfecto</span>
+        <v-card class="elevation-2" :color="$const.ACCENT_COLOR">
+          <v-card-title class="headline">Lorem ipsum</v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline> -->
+
+      </v-card>
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  components: {
+    ActionButton: () => import('@/components/atoms/ActionButton')
+  },
+  props: {
+    drawer: {
+      type: Boolean,
+      default: false
+    },
+    messages: {
+      type: Array,
+      default: false
+    }
+  }
+}
+</script>
