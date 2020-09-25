e7df007d510389e71dfb0f768e9a23e1cc52b606
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-09-25 21:53:28 +0900

    [update]user information icon
    story追加

--- client/components/molecules/UserInformationIcon.stories.js
+++ client/components/molecules/UserInformationIcon.stories.js
@@ -22,6 +22,13 @@ export const Default = Template.bind({})
 Default.args = {
   user: {
     id: 1,
-    num: 56
+    name: "ピコピコぽん太郎",
+    profile: "先進理工学部二郎系学科3年生です。東京都西早稲田駅近くにある二郎系ラーメン店ピコピコポンが激推しです。おいしいよ。",
+    img: "https://storage.googleapis.com/remollege-storage/1599218148359sampleIcon1.jpg",
   }
+}
+
+export const NoUsers = Template.bind({})
+NoUsers.args = {
+  user: null
 }
